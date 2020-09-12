43cb32d1c64d0336c8f2a35d8a684a6db8cd0b73
Omar Omar <oomar@umich.edu>
2020-09-12 14:14:14 -0400

    Main Menu Basic Implementaiton

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Play()
+    {
+        // Go to Game Scene
+        Debug.Log("Activating Game Scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void Exit()
+    {
+        Debug.Log("Quiting Game");
+        Application.Quit();
+    }
+}
