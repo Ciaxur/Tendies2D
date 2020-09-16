6ef2707f6cc5cc74cf28df3b6cac3fe02599244e
MuazAlhaidar <omunam10@gmail.com>
2020-09-16 19:14:48 -0400

    Added Some components
    Fullscreen mode
    Volume sliders are attached to mixers

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public bool windowedState = false;
-
-    public void WindowedMode()
+    public void SetFullscreen(bool isFullscreen)
     {
-        if (!windowedState)
-        {
-            Debug.Log("Windowed Mode Activated");
-            windowedState = !windowedState;
-        }
-        else
-        {
-            Debug.Log("Windowed Mode Deactivated");
-            windowedState = !windowedState;
-        }
+        Screen.fullScreen = isFullscreen;
+    }
 
+    public AudioMixer audioMixer;
+
+    public float minVolume = 0.00f;
+    public float maxVolume = 100.00f;
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        audioMixer.SetFloat("effectsVolume", volume);
     }
 }
