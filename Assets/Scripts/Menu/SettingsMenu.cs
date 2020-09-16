f993868c005395667039695666725ab69129b383
MuazAlhaidar <omunam10@gmail.com>
2020-09-16 17:37:06 -0400

    Added Settings menu script

---
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public bool windowedState = false;
+
+    public void WindowedMode()
+    {
+        if (!windowedState)
+        {
+            Debug.Log("Windowed Mode Activated");
+            windowedState = !windowedState;
+        }
+        else
+        {
+            Debug.Log("Windowed Mode Deactivated");
+            windowedState = !windowedState;
+        }
+
+    }
+}
