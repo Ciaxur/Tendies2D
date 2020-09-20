be376807bb5b9e990ec9202df094b5a645e9acf8
Omar Omar <oomar@umich.edu>
2020-09-20 14:46:32 -0400

    📦 Infinite Platform Spawing
    - PlatformHandler takes care of all Platform Spawning and Despawning
    - Added some Character Sprites

---
+++ Assets/Scripts/PlatformHandler.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformHandler : MonoBehaviour
+{
+    // External Reference Resources
+    public GameObject platformPrefab;
+    public GameObject player;
+    public World world;
+    
+    // Distance in Relation to Player and/or Platform
+    public Vector2  maxDist             = new Vector2(5.0f, 10.0f);
+    public Vector2  minDist             = new Vector2(0.0f, 2.0f);
+    public float    platformSpacing     = 4.0f;
+
+    // Internal Platform Resources
+    private Queue<GameObject> platforms = new Queue<GameObject>();
+    private int latestPlatformCount = 4;                                    // Last N-Platforms
+    private Queue<GameObject> latestPlatforms = new Queue<GameObject>();    // Keeps track of Last N-Plaforms
+    private GameObject lastPlaform;                                         // Keeps track of the Last Spawned Platform
+
+    
+    
+    /**
+     * Helper Function
+     *  Sets Platform Collision to a State relative to the
+     *  collider given.
+     *
+     * @param state - The State to set Collision to
+     * @param collider - The Collider to Set Collision State onto
+     */
+    public void setPlatformCollision(bool state, Collider2D collider) {
+        Collider2D playerCollider = collider;
+        foreach (GameObject platform in this.platforms)
+            Physics2D.IgnoreCollision(platform.GetComponent<Collider2D>(), playerCollider, !state);
+    }
+
+    /** 
+     * Spawns a Platform in respect to given Postition
+     * @param pos Relative Position
+     */
+    private void spawnPlatform(Vector2 pos)
+    {
+        // Get Random Location for Platform
+        Vector2 platformPosition = world.getRandomLocationFrom(pos);
+
+        // Make sure Spacing is Accurate
+        foreach (GameObject platform in this.platforms)
+        {
+            float dist = Vector2.Distance(platformPosition, platform.transform.position);
+            if(dist < this.platformSpacing)
+            {
+                // Try a better Approach
+                spawnPlatform(platform.transform.position);
+                return;
+            }
+        }
+
+        GameObject obj = Instantiate(platformPrefab, platformPosition, Quaternion.identity);
+        obj.transform.parent = this.transform;
+        platforms.Enqueue(obj);
+
+        // Keep track of Latest Platforms
+        latestPlatforms.Enqueue(obj);
+        if (latestPlatforms.Count > latestPlatformCount)
+            latestPlatforms.Dequeue();
+        
+        // Keep track of the Last Platform
+        lastPlaform = obj;
+    }
+
+    /** Runs Platform Spawning Algorithm */
+    public void runPlatformSpawns() {
+        // Convert last Platforms to Array
+        GameObject[] plats = latestPlatforms.ToArray();
+        GameObject relObj;
+        
+        // Initially include Player
+        if (latestPlatforms.Count < latestPlatformCount) {
+            // Relative to Player Chance if other Plats are there (25% Chance)
+            if (latestPlatforms.Count == 0 || Random.Range(0.0f, 1.0f) < 0.25f) {
+                relObj = player;
+            }
+            else {
+                // Pick & Spawn Relative to Random Platform
+                GameObject randPlat = plats[Mathf.FloorToInt(Random.Range(0.0f, plats.Length))];
+                relObj = randPlat;
+            }
+        }
+
+        // Spawn Relative to last N-Platforms
+        else {
+            relObj = plats[Mathf.FloorToInt(Random.Range(0.0f, plats.Length))];
+        }
+
+        // Check for Integrity of Platform
+        if(relObj == null) {    // Default to Player if Null
+            relObj = player;
+        }
+
+        // Spawn to Relative Selected Position
+        spawnPlatform(relObj.transform.position);
+    }
+    
+    /**
+     * Removes the last N-Platforms
+     * @param numPlats The number of Platforms to remove
+     */
+    public void destroyLast(int numPlats = 1) {
+        for(int i = 0; i < numPlats; i++) {
+            if (platforms.Count == 0) // Exit if Empty
+                return;
+            Destroy(platforms.Dequeue());
+        }
+    }
+
+    /**
+     * Removes all the Platforms
+     */
+    public void clearPlatforms() {
+        foreach(GameObject platform in platforms)
+            Destroy(platform);
+        platforms.Clear();
+        latestPlatforms.Clear();
+    }
+
+    /** Returns the Last Platform */
+    public GameObject getLastPlatform() {
+        return lastPlaform;
+    }
+
+    /** Returns the First Platform */
+    public GameObject getFistPlatform() {
+        return platforms.Peek();
+    }
+
+    /** Returns the Total Number of Platforms */
+    public int getTotalPlatforms() {
+        return platforms.Count;
+    }
+    
+}
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,98 +4,77 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
-    // External Resources
-    public GameObject platformPrefab;
+    // External Reference Resources
     public GameObject player;
-
-    // Distance in Relation to Player and/or Platform
-    public Vector2  maxDist             = new Vector2(5.0f, 10.0f);
-    public Vector2  minDist             = new Vector2(0.0f, 2.0f);
-    public float    platformSpacing     = 4.0f;
+    public PlatformHandler platformHandler;
 
     // Environment Boundaries
     public float  leftBoundaryX = -10.0f;
     public float  rightBoundaryX = 10.0f;
 
-    // Internal Resources
-    private Queue<GameObject> platforms = new Queue<GameObject>();
-    private bool spaceUp = true;        // State of Space Key
-    private bool keyD = true;           // State of D Key
+    // Infinite Spawning Resources
+    public float  dyOffScreen = -5.0f;      // Distance Y Relative to Player that is Offscreen
+    public float  dyLookAhead = 20.0f;      // Distance Y Relative to Player to look Ahead to Pre-Spawn/Handle
 
     // Internal Player References
     private Rigidbody2D playerRbody2D;
+    public GameObject debugMenu;
 
 
-    // Random Location from given Position
-    private Vector2 getRandomLocationFrom(Vector2 pos)
-    {
+    /**
+     * Random Location from given Position
+     * @param pos Relative Position
+     */
+    public Vector2 getRandomLocationFrom(Vector2 pos) {
         int xDirection = Random.Range(0.0f, 1.0f) > 0.5 ? 1 : -1;
 
         // Constrain X within Boundaries
         float x = Mathf.Clamp(
-            pos.x + (xDirection * Random.Range(0.0f, this.maxDist.x - this.minDist.x)),
+            pos.x + (xDirection * Random.Range(0.0f, platformHandler.maxDist.x - platformHandler.minDist.x)),
             leftBoundaryX, rightBoundaryX
         );
-        float y = (pos.y + this.minDist.y) + Random.Range(0.0f, this.maxDist.y - this.minDist.y);
+        float y = (pos.y + platformHandler.minDist.y) + Random.Range(0.0f, platformHandler.maxDist.y - platformHandler.minDist.y);
         return new Vector2(x, y);
-    }
-
-    // Spawns a Platform in respect to given Postition
-    public void spawnPlatform(Vector2 pos)
-    {
-        // Get Random Location for Platform
-        Vector2 platformPosition = getRandomLocationFrom(pos);
-
-        // Make sure Spacing is Accurate
-        foreach (GameObject platform in this.platforms)
-        {
-            float dist = Vector2.Distance(platformPosition, platform.transform.position);
-            if(dist < this.platformSpacing)
-            {
-                // Try a better Approach
-                spawnPlatform(platform.transform.position);
-                return;
-            }
-        }
-
-        GameObject obj = Instantiate(platformPrefab, platformPosition, Quaternion.identity);
-        obj.transform.parent = this.transform;
-        this.platforms.Enqueue(obj);
-    }
-
-    // Helper Function: Sets Platform Collision
-    private void setPlatformCollision(bool state) 
-    {
-        Collider2D playerCollider = this.playerRbody2D.GetComponent<Collider2D>();
-        foreach (GameObject platform in this.platforms)
-            Physics2D.IgnoreCollision(platform.GetComponent<Collider2D>(), playerCollider, !state);
     }
 
 
     // Physics Update
-    void FixedUpdate() 
-    {
+    void FixedUpdate() {
         // Platform Collision based on Player Movement
         if (this.playerRbody2D.velocity.y < 0) {
-            this.setPlatformCollision(true);
+            platformHandler.setPlatformCollision(true, this.playerRbody2D.GetComponent<Collider2D>());
         } 
         else if (this.playerRbody2D.velocity.y > 0) {
-            this.setPlatformCollision(false);
+            platformHandler.setPlatformCollision(false, this.playerRbody2D.GetComponent<Collider2D>());
         }
     }
 
+    // Last Minute Checks
+    void LateUpdate() {
+        // Despwan Off-Screen Objects
+        Vector2 distFirst = platformHandler.getFistPlatform().transform.position - player.transform.position;
+        if (distFirst.y < dyOffScreen)
+            platformHandler.destroyLast();
+
+        // Handle Spawning Ahead
+        Vector2 distLast = platformHandler.getLastPlatform().transform.position - player.transform.position;
+        if (distLast.y < dyLookAhead)
+            platformHandler.runPlatformSpawns();
+
+        // DEBUG: Information
+        debugMenu.GetComponent<TMPro.TextMeshProUGUI>().text = 
+            $"Last Platform: {distFirst.y:N2}\n" +
+            $"First Platform: {distLast.y:N2}\n" +
+            $"Total Platforms: {platformHandler.getTotalPlatforms()}";
+    }
+
     // Start is called before the first frame update
-    void Start()
-    {
-        // Spawn Initial Platforms
-        spawnPlatform(player.transform.position);
+    void Start() {
+        // Spawn Initial 10 Platforms
+        for (int i = 0; i < 10; i++)
+            platformHandler.runPlatformSpawns();
 
         // Assign Player References
         this.playerRbody2D = this.player.GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 }
