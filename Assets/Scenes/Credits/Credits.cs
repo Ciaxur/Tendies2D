3fe7adb18a3ae029ddc88d0ec45c888b88fa21bd
Omar Omar <oomar@umich.edu>
2020-10-10 15:25:08 -0400

    Menu Modificaitons & Exit Credits

---
+++ Assets/Scenes/Credits/Credits.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+    // External References
+    public MainMenu mainMenu;
+    
+    public void Exit() {
+        mainMenu.Exit();
+    }
+}
--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    // External References
+    public Animator creditsAnimation;
 
     public void Play() {
         // Go to Game Scene
         Debug.Log("Activating Game Scene");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    IEnumerator PlayCredits() {
+        creditsAnimation.SetTrigger("Run");
+        yield return new WaitForSeconds(5f);
+    }
+
+    // Plays Credits then Exits
+    public void PlayExit() {
+        StartCoroutine(PlayCredits());
+    }
     
+    // Actually Quits the Game
     public void Exit() {
         Debug.Log("Quiting Game");
         Application.Quit();
