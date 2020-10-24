ab9389a8259c6ca498744c86461079ca45b8dd29
Omar Omar <oomar@umich.edu>
2020-10-24 13:06:30 -0400

    Timer Animations

--- Assets/Scripts/Game Timer/GameTimer.cs
+++ Assets/Scripts/Game Timer/GameTimer.cs
@@ -12,6 +12,7 @@ public class GameTimer : MonoBehaviour
     public Text timerText;
     public float initialTimer = 5f;
     public bool timerStarted = false;
+    public Animator animator;
 
     // Time Increases
     public float timeOnProgress = 5f;
@@ -26,12 +27,37 @@ public class GameTimer : MonoBehaviour
     }
     
 
+    // Colorz
+    Color green = new Color(0.505f, 0.875f, 0.266f, 1f);
+    Color red = new Color(0.914f, 0.266f, 0.266f, 1f);
+    Color yellow = new Color(0.890f, 0.890f, 0.266f, 1f);
+    Color white = new Color(1f, 1f, 1f, 1f);
+    bool isAnimPlay = false;
+    
+    // Runs animation for exmpanding Timer
+    // Changes Color from 1 -> 2
+    IEnumerator expandTimer(Color color1, Color color2) {
+        isAnimPlay = true;
+        
+        timerText.color = color1;
+        animator.SetTrigger("ExpandTimer");
+        yield return new WaitForSeconds(0.4f);
+        timerText.color = color2;
+
+        isAnimPlay = false;
+    }
+
     // Public method to Add based on Type to the Timer
     public void addToTime(GameTimer.TYPE timeType) {
+        if (!isAnimPlay) {
+            Color currentColor = timerText.color;
+            StartCoroutine(expandTimer(green, currentColor));
+        }
+
         switch(timeType) {
             case TYPE.ENEMY_KILL:
                 timer += timeOnEnemyKill;
-            break;
+                break;
             case TYPE.PROGESS:
                 timer += timeOnProgress;
                 break;
@@ -49,6 +75,24 @@ public class GameTimer : MonoBehaviour
     // Set the Time Text
     void LateUpdate() {
         setTimerText(timer);
+
+        // Check Timer Ranges & Set Color
+        if (!isAnimPlay) {
+            // Panic Time!
+            if (timer < 5.0f) {
+                StartCoroutine(expandTimer(red, red));
+            }
+
+            // Warning Time
+            else if (timer < 7.5f) {
+                timerText.color = yellow;
+            }
+
+            // Safe Time
+            else {
+                timerText.color = white;
+            }
+        }
     }
 
     // Update the Time
